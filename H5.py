aff6d436c5da909a6f1524e11aed73d9f5a9f0a4
daniel0809 <31154340+dnaiel0809@users.noreply.github.com>
2023-02-03 00:13:49 +0800

    Update and rename H5Copy1.py to H5.py

---
+++ H5.py
@@ -0,0 +1,97 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import torchvision
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+from functions import *
+from ALSTM import *
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.metrics import accuracy_score
+import pickle
+from torch.optim.lr_scheduler import StepLR
+from collections import OrderedDict
+import time
+from torch.utils.data import random_split
+import h5py
+from torch.utils.data import WeightedRandomSampler
+
+
+
+
+use_cuda = torch.cuda.is_available()   
+res_size = 224        # ResNet image size
+data_path = "../../CHI_data/raw_frame_P"    # define image RGB data path
+begin_frame, end_frame, skip_frame = 1, 7, 1
+batch_size = 288
+num_workers = 32
+
+
+csv_path =  '../CHI_PhoneLog_gap.csv'
+user = ['P10','P11','P16','P17','P18','P19','P23','P24','P25','P26','P28','P29','P30','P31','P32','P33','P35','P36','P37','P38','P40','P41','P42','P43','P44','P45','P46','P47','P48','P49','P50','P51','P52','P53','P54','P55'] 
+
+df = pd.read_csv(csv_path, index_col="file_name", low_memory=False)
+df = df[df['kill_time'].notna()]
+df.kill_time = df.kill_time.astype(int)
+df = df[df['PID'].isin(user)]
+df = df.drop(columns=['Unnamed: 0', 'PID', 'screen_status', 'record_time', 'is_screenshotted','kill_time', 'cdma_signal_strength', 'lte_signal_strength_dbm']) #8
+
+
+dataset_X_path = "./dataset/CHI_images_users_by_day_55_weekend_"+dataset+"Set_X_fold"+fold+".npy"
+dataset_Y_path = "./dataset/CHI_images_users_by_day_55_weekend_"+dataset+"Set_Y_fold"+fold+".npy"
+h5_path = "../../../../work/u7002555/CHI_images_users_by_day_55_"+k+"_weekend_random_"+dataset+"Set_fold"+fold+".h5"
+
+
+
+all_X_list = np.load(dataset_X_path)
+all_y_list = np.load(dataset_Y_path)
+np.random.shuffle(all_y_list)
+transform = transforms.Compose([transforms.Resize([res_size, res_size]),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+
+
+selected_frames = {'begin': 1, 'end': 7, 'skip': 1}# data_set = Dataset_CRNN_varlen(data_path, all_X_list, all_y_list, selected_frames, df, transform=transform)
+try:
+    data_set = Dataset_CRNN_Log_varlen(data_path, all_X_list, all_y_list, selected_frames,df, transform=transform)
+except:
+    continue
+params = {'batch_size': batch_size
+          , 'num_workers': num_workers, 'pin_memory': True
+#           , 'drop_last': True
+          , 'shuffle': True
+#           , 'sampler': sampler
+         } if use_cuda else {}
+train_loader = data.DataLoader(data_set, **params)
+
+ff = h5py.File(h5_path, mode='w')
+
+ff.create_dataset("img", (len(data_set),7,3,224,224), dtype=np.float32)
+ff.create_dataset("log", (len(data_set),7,183), dtype=np.float32)
+ff.create_dataset("y", (len(data_set),1), dtype=np.float32)
+ff.create_dataset('path', (len(data_set),1),'S70')
+ff.create_dataset("lengths", (len(data_set),1), dtype=np.float32)
+filname = []
+print('len',len(data_set))
+for batch_idx, (X, X_log, lengths, y, path) in tqdm(enumerate(train_loader)):
+    for i in range(batch_size):
+        index = batch_idx*batch_size+i
+        if index >= len(data_set):
+            break
+        ff["img"][index, ...] = X[i].numpy()
+        ff["log"][index, ...] = X_log[i].numpy()
+        ff["y"][index, ...] = y[i].numpy()
+        ff["lengths"][index, ...] = lengths[i].numpy()
+        ff["path"][index, ...] = np.expand_dims(path[i].encode("ascii"), axis=0)
+
+
+ff.close()
+print(h5_path)
+
